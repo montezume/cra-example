de4d6ea4691bc70403218e845abb56703b69a059
Malcolm Laing <malcolmclaing@gmail.com>
2017-09-17 19:18:13 -0400

    Change index

--- index.js
+++ index.js
@@ -7,22 +7,16 @@ const middlewares = jsonServer.defaults();
 const fs = require('fs');
 const port = process.env.PORT || 3001;
 
-// server.use(middlewares);
-// server.use(router);
-
 server.use('/api', router);
 
-// if (!process.env.NODE_ENV === 'production') {
+if (!process.env.NODE_ENV === 'production') {
   server.use(express.static(path.resolve(__dirname, 'build')));
-// }
-
+}
 
 // Always return the main index.html, so react-router render the route in the client
 server.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
-
-// server.use('/api', router);
 
 server.listen(port, function () {
   console.log('\x1b[36mjson-server is running on port: ' + port);
